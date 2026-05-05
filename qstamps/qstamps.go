94e1c4df9a23625ea2988e715bcc20bbae09340e
kitsne241 <169665926+kitsne241@users.noreply.github.com>
2026-05-05 17:08:55 +0900

    qstamps 仕様更新

--- qstamps/qstamps.go
+++ qstamps/qstamps.go
@@ -11,7 +11,7 @@ import (
 type QStamps struct {
 	bot         *wsbot.Bot
 	stampNameID map[string]string
-	stampIDName map[string]string
+	stampIDData map[string]traq.Stamp
 }
 
 // 引数の Bot をもとにインスタンスを生成
@@ -26,21 +26,38 @@ func New(bot *wsbot.Bot) (*QStamps, error) {
 
 // traQ の直近の全スタンプの名前と ID の対応表を取得
 func (q *QStamps) Refresh() error {
-	stamps, _, err := q.bot.API().StampAPI.GetStamps(context.Background()).Execute()
+	stampsWithThumbnail, _, err := q.bot.API().StampAPI.GetStamps(context.Background()).Execute()
 	if err != nil {
 		return err
 	}
 
+	// スタンプには謎に型が Stamp と StampWithThumbnail の 2 つある
+	// とりあえずシンプルな方を使ってみて、hasThumbnail が思ったより重要だと分かった暁には乗り換えてみる
+
+	stamps := make([]traq.Stamp, len(stampsWithThumbnail))
+
+	for i, stampWithThumbnail := range stampsWithThumbnail {
+		stamps[i] = traq.Stamp{
+			Id:        stampWithThumbnail.Id,
+			Name:      stampWithThumbnail.Name,
+			CreatorId: stampWithThumbnail.CreatorId,
+			CreatedAt: stampWithThumbnail.CreatedAt,
+			UpdatedAt: stampWithThumbnail.UpdatedAt,
+			FileId:    stampWithThumbnail.FileId,
+			IsUnicode: stampWithThumbnail.IsUnicode,
+		}
+	}
+
 	stampNameID := map[string]string{}
-	stampIDName := map[string]string{}
+	stampIDData := map[string]traq.Stamp{}
 
 	for _, stamp := range stamps {
-		stampIDName[stamp.Id] = stamp.Name
+		stampIDData[stamp.Id] = stamp
 		stampNameID[stamp.Name] = stamp.Id
 	}
 
 	q.stampNameID = stampNameID
-	q.stampIDName = stampIDName
+	q.stampIDData = stampIDData
 
 	return nil
 }
@@ -51,10 +68,28 @@ func (q *QStamps) GetStampID(name string) (string, bool) {
 	return stampID, ok
 }
 
+// 引数の ID をもつスタンプの現在のデータを取得
+func (q *QStamps) GetStamp(id string) (traq.Stamp, bool) {
+	stamp, ok := q.stampIDData[id]
+	return stamp, ok
+}
+
 // 引数の ID をもつスタンプの現在の名前を取得
 func (q *QStamps) GetStampName(id string) (string, bool) {
-	stampName, exists := q.stampIDName[id]
-	return stampName, exists
+	stamp, ok := q.GetStamp(id)
+	if !ok {
+		return "", false
+	}
+	return stamp.Name, true
+}
+
+// 引数の名前をもつスタンプの現在のデータを取得
+func (q *QStamps) GetStampByName(name string) (traq.Stamp, bool) {
+	stampID, ok := q.GetStampID(name)
+	if !ok {
+		return traq.Stamp{}, false
+	}
+	return q.GetStamp(stampID)
 }
 
 // メッセージに引数の名前のスタンプを順番につける。返り値は成功したスタンプの名前の配列
@@ -63,7 +98,7 @@ func (q *QStamps) Stamp(messageID string, stampNames ...string) ([]string, error
 	successfulStamps := []string{}
 
 	for _, name := range stampNames {
-		stampID, ok := q.stampNameID[name]
+		stampID, ok := q.GetStampID(name)
 		if !ok {
 			err = fmt.Errorf("スタンプ :%s: はキャッシュに存在しません", name)
 			break
